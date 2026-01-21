fix: add a scaling factor = 3/2 to the scanner to correct its readings

r2scanner.py
@@ -39,7 +39,7 @@ class Scanner(Node):
         lr2i = np.nanargmin(laser_range)
 
         # log the info
-        self.get_logger().info('Shortest distance at %i degrees' % lr2i)
+        self.get_logger().info('Shortest distance at %i degrees' % (lr2i*3/2))
 
 
 def main(args=None):