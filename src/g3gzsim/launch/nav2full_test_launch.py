975e0b4a3a3839564634c0d50fa2843aa864e40f
Chesium <chesium@hotmail.com>
2026-03-17 16:45:06 +0800

    feat: copy the nav2 param and rviz config

--- src/g3gzsim/launch/nav2full_test_launch.py
+++ src/g3gzsim/launch/nav2full_test_launch.py
@@ -5,6 +5,8 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
+# colcon build --packages-select g3gzsim && roset && ros2 launch g3gzsim nav2full_test_launch.py
+
 def generate_launch_description():
 
     nav2_bringup_dir = get_package_share_directory("nav2_bringup")
@@ -12,12 +14,18 @@ def generate_launch_description():
         nav2_bringup_dir, "launch", "tb3_simulation_launch.py"
     )
 
+    g3gzsim_dir = get_package_share_directory("g3gzsim")
+    nav2_params_file = os.path.join(g3gzsim_dir, "nav2", "nav2_params.yaml")
+    rviz_config_file = os.path.join(g3gzsim_dir, "rviz", "nav2_default_view.rviz")
+
     tb3_simulation_launch_action = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(tb3_simulation_launch_file),
         launch_arguments={
             "headless": "False",
             "slam": "True",
             "map": "",
+            "params_file": nav2_params_file,
+            "rviz_config_file": rviz_config_file,
         }.items(),
     )
 
